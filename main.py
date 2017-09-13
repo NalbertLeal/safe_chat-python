786e62de60b31586cdb612203d8b4ab11fb5af36
nalbertg <nalbertg@outlook.com>
2017-09-13 02:22:41 -0300

    main argv and identation fixed

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 
 if __name__ == '__main__':
 
-from rc4 import RC4
-from server import Server
-from client import Client
-from os import args
+    from rc4 import RC4
+    from server import Server
+    from client import Client
+    import sys
 
-if len(args) > 1:
-    username = args[1]
-    c = Client(username)
-    c.run()
-else:
-    s = Server()
-    s.run()
+    if len(sys.argv) > 1:
+        username = os.argv[1]
+        c = Client(username)
+        c.run()
+    else:
+        print('>> Initiating the server')
+        s = Server()
+        s.run()
