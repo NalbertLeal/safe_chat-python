792659ab08e819b3f2621c1f5d453244a23e567f
estherbarbara <tekmedeiros@gmail.com>
2017-09-14 19:19:12 -0300

    rc4 pronto

--- rc4.py
+++ rc4.py
@@ -1,32 +1,46 @@
 
 class RC4():
-    def __init__(self):
-        self.s = []
-        self.q = None
-        self.p = None
 
-    def key(self, key):
-        """ RC4 KEy algorithm (KSA) """
-        self.s = [n for n in range(0, 256)]
-        self.p = 0
-        self.q = 0
+    def __init__(self, k):
+        key = []
+        K = []
+        self.PT = []
+        self.S = []
+        self.TempS = []
+
+        #passando a key de string de binarios para uma lista de binarios
+        for i in range(0, len(k)):
+            key.append(int(k[i]))
+
+        T = [] #temp, auxiliar
+        #Inicializando S e T (state e temp)
+        for i in range(0, 256):
+            #S de 0 ate 255
+            self.S.append(i)
+            #T com os elementos da chave, com repeticao ou não(este apenas para o caso da chave ter tamanho 256)
+            T.append(key[i%len(key)])
+
         j = 0
-        for i in range(256):
-            if len(key) > 0:
-                j = (j + self.s[i] + int(key[i % len(key)]) ) % 256
-            else:
-                j = (j + self.s[i] + int(key[i % len(key)]) ) % 256
-            self.s[i], self.s[j] = self.s[j], self.s[i]
+        #Permutação inicial em S
+        for i in range(0, 256):
+            j = (j + self.S[i] + T[i])%256
+            #swap de duas posicoes
+            self.S[i], self.S[j] = self.S[j], self.S[i]
+    
+    def code(self, message):
 
-    def byte_generator(self):
-        """ """
-        self.p = (self.p + 1) % 256
-        self.q = (self.q + self.s[self.p]) % 256
-        self.s[self.p], self.s[self.q] = self.s[self.q], self.s[self.p]
-        return self.s[(self.s[self.p] + self.s[self.q]) % 256]
+        i = 0
+        j = 0
+        K = ''
+        TempS = self.S[:]
+        for char in message:
+            i = (i + 1)%256
+            j = (j + self.S[i])%256
+            #swap de duas posicoes
+            TempS[i], TempS[j] = TempS[j], TempS[i]
 
-    def encript(self, input_string):
-        return  [ord(i) ^ self.byte_generator() for i in input_string]
-
-    def decript(self, input_byte_string):
-        return "".join([chr(i ^ self.byte_generator()) for i in input_byte_string])
+            t = (TempS[i]+TempS[j])%256
+            k = TempS[t]
+            #K resultando possui a mensagem encriptada em string
+            K = K + ( chr(ord(char) ^ k) )
+        return K
